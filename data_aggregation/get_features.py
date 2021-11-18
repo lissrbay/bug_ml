c253c0f032a131543c838d246e9f1eed25b1764f
akhvorov <khvorov.aleksandr@gmail.com>
2021-11-18 15:06:13 +0300

    Many small refactorings for API

--- data_aggregation/get_features.py
+++ data_aggregation/get_features.py
@@ -1,9 +1,11 @@
 import re
 import json
 import os
+from typing import Dict, Any
+
 from tqdm import tqdm
 import pickle
-import pandas as pd 
+import pandas as pd
 
 
 class FeatureExtractor:
@@ -23,64 +25,64 @@ class FeatureExtractor:
         self.is_parallel = []
         self.report_id = []
 
-    def load_code_data(self, path_to_code, path_to_report_ids):
-        self.reports_code=pickle.load(open(path_to_code, "rb"))
-        self.reports_ids=pickle.load(open(path_to_report_ids, "rb"))
-        
+    @staticmethod
+    def load_code_data(path_to_code: str, path_to_report_ids: str):
+        reports_code = pickle.load(open(path_to_code, "rb"))
+        reports_ids = pickle.load(open(path_to_report_ids, "rb"))
+        return reports_code, reports_ids
 
     def get_features_from_files(self, path_to_reports, path_to_code, path_to_report_ids):
-        self.load_code_data(path_to_code, path_to_report_ids)
-        for report_id in tqdm(self.reports_ids):
-            report_path = open(os.path.join(path_to_reports, str(report_id)+".json"), 'r')
+        reports_code, reports_ids = self.load_code_data(path_to_code, path_to_report_ids)
+        for report_id in tqdm(reports_ids):
+            report_path = open(os.path.join(path_to_reports, str(report_id) + ".json"), 'r')
             report = json.load(report_path)
-            for i, frame in enumerate(report['frames'][:80]):   
+            for i, frame in enumerate(report['frames'][:80]):
                 frame['class'] = report['class']
                 frame['pos'] = i
                 frame['id'] = report_id
-                self.get_feature_from_code(self.reports_code[report_id][i])
-                self.get_feature_from_metadata(frame)
-        
+                self.add_feature_from_code(reports_code[report_id][i])
+                self.add_feature_from_metadata(frame)
 
-    def get_feature_from_code(self, method_code):
+    def add_feature_from_code(self, method_code: str) -> 'FeatureExtractor':
         self.method_tokens_count.append(len(method_code.split()))
         self.newObjectsCount.append(len(re.findall(r' new ', method_code)))
         self.has_no_code.append(len(method_code) == 0)
         self.method_len.append(len(method_code))
-    
-    def get_feature_from_metadata(self, method_meta):
+        return self
+
+    def add_feature_from_metadata(self, method_meta: Dict[str, Any]) -> 'FeatureExtractor':
         self.exception_class.append(method_meta['class'])
         method_name = method_meta['method_name']
         self.has_runs.append("run" in method_name)
         self.has_dollars.append("$" in method_name)
-        self.is_parallel.append("Thread"  in method_name)
-        self.method_file_position.append(method_meta['line_number'])
+        self.is_parallel.append("Thread" in method_name)
+        self.method_file_position.append(int(method_meta['line_number']))
         self.is_java_standart.append(method_name[:4] == 'java')
         self.method_stack_position.append(method_meta['pos'])
         self.label.append(method_meta['label'])
         self.report_id.append(method_meta['id'] if 'id' in method_meta else 0)
         self.method_name.append(method_name)
+        return self
 
-    def to_pandas(self):
-        df = pd.DataFrame({ "is_java_standart":self.is_java_standart,
-            'method_len':self.method_len,
-            'method_tokens_count':self.method_tokens_count,
-            'method_stack_position':self.method_stack_position,
-            'method_file_position':self.method_file_position,
-            'exception_class':self.exception_class,
-            'has_runs':self.has_runs,
-            'has_dollars':self.has_dollars,
-            'method_name':self.method_name,
-            'label':self.label,
-            'has_no_code':self.has_no_code,
-            'newObjectsCount':self.newObjectsCount,
-            'is_parallel':self.is_parallel,
-            "report_id":self.report_id})
+    def to_pandas(self) -> pd.DataFrame:
+        df = pd.DataFrame({"is_java_standart": self.is_java_standart,
+                           'method_len': self.method_len,
+                           'method_tokens_count': self.method_tokens_count,
+                           'method_stack_position': self.method_stack_position,
+                           'method_file_position': self.method_file_position,
+                           'exception_class': self.exception_class,
+                           'has_runs': self.has_runs,
+                           'has_dollars': self.has_dollars,
+                           'method_name': self.method_name,
+                           'label': self.label,
+                           'has_no_code': self.has_no_code,
+                           'newObjectsCount': self.newObjectsCount,
+                           'is_parallel': self.is_parallel,
+                           "report_id": self.report_id})
         return df
 
-
     def save_features(self, save_path):
-        if self.exception_class == []:
+        if not self.exception_class:
             print("You should run get features methods first.")
             return
-        self.df = self.to_pandas()
-        self.df.to_csv(save_path)
+        self.to_pandas().to_csv(save_path)
--- data_aggregation/union_predictions_and_features.py
+++ data_aggregation/union_predictions_and_features.py
@@ -1,46 +1,47 @@
 import pandas as pd
-import sys
 import os
 import pickle
 import argparse
-from collections import Counter
-
 
 parser = argparse.ArgumentParser(description='Union features from sources')
 parser.add_argument('data_path', type=str)
 parser.add_argument('save_path', type=str)
 
 
-
-def make_indices(df_features):
-    df_features['indices'] = df_features['report_id'].apply(lambda x: str(x)+'_') + df_features['method_stack_position'].apply(lambda x: str(x))
+def add_indices(df_features: pd.DataFrame) -> pd.DataFrame:
+    df_features['indices'] = df_features['report_id'].apply(lambda x: str(x) + '_') + df_features[
+        'method_stack_position'].apply(lambda x: str(x))
     return df_features
 
 
-def union_preds_features(preds, df_features):
-    df_features = make_indices(df_features)
-    preds = make_indices(preds)
+def union_preds_features(preds: pd.DataFrame, df_features: pd.DataFrame) -> pd.DataFrame:
+    df_features = add_indices(df_features)
+    preds = add_indices(preds)
     df_features['indices'] = df_features['indices'].astype(str)
     preds['indices'] = preds['indices'].astype(str)
     preds = preds.drop(['report_id'], axis=1)
 
     df_features = df_features.merge(preds, on='indices', how='inner')
     return df_features
-    
-        
-if __name__ == "__main__":
+
+
+def main():
     args = parser.parse_args()
 
-    df = pd.read_csv(os.path.join(args.data_path,"lstm_prediction.csv"),index_col=0)
-    df_features = pd.read_csv(os.path.join(args.data_path,"reports_features.csv"),index_col=0)
+    df = pd.read_csv(os.path.join(args.data_path, "lstm_prediction.csv"), index_col=0)
+    df_features = pd.read_csv(os.path.join(args.data_path, "reports_features.csv"), index_col=0)
     reports_code = pickle.load(open(os.path.join(args.data_path, "reports_code"), "rb"))
-    reports_ids = pickle.load(open(os.path.join(args.data_path,"reports_ids"), "rb"))
+    reports_ids = pickle.load(open(os.path.join(args.data_path, "reports_ids"), "rb"))
 
-    df_features = make_indices(df_features)
-    df = make_indices(df)
+    df_features = add_indices(df_features)
+    df = add_indices(df)
 
     df_features['indices'] = df_features['indices'].astype(str)
-    df['indices'] = df['indices'].astype(str)  
+    df['indices'] = df['indices'].astype(str)
     df_features = df_features.merge(df, on='indices', how='inner')
 
     df_features.to_csv(os.path.join(args.save_path))
+
+
+if __name__ == "__main__":
+    main()
