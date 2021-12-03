8f743c4a352c308dfeb079c37ba78c943ca55d52
lissrbay <rbayliss964@gmail.com>
2021-12-03 17:01:39 +0300

    add scaffle

--- new/model/frame_encoders/scaffle.py
+++ new/model/frame_encoders/scaffle.py
@@ -2,11 +2,31 @@ import torch
 
 from new.data.report import Frame
 from new.model.frame_encoders.frame_encoder import FrameEncoder
+from gensim.models import Word2Vec
+import numpy as np
+
+def clean_method_name(method_name):
+    method_name = method_name.replace('lambda$', '')
+    method_name = method_name.replace('$0', '')
+    method_name = method_name.replace('$', '')
+    return method_name
 
 
 class ScaffleFrameEncoder(FrameEncoder):
+    def __init__(self, name):
+        self.emb_model = Word2Vec.load(name)
+
     def encode(self, frame: Frame) -> torch.Tensor:
-        pass
+        method_name_tokens = clean_method_name(frame.name).split('.')
+        embeddings = []
+        for word in method_name_tokens:
+            try:
+                vector = self.emb_model.wv[word].reshape((100,))
+            except KeyError:
+                vector = np.zeros((100,))
+            embeddings.append(vector)
+        return torch.FloatTensor(embeddings)
+
 
     def dim(self) -> int:
-        pass
+        return self.emb_model.wv.vectors.shape[1]
