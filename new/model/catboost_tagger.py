cfd72506958cf92d31684190c51ae450d2e73c2d
lissrbay <rbayliss964@gmail.com>
2022-04-11 16:22:53 +0300

    fx features splitting

--- new/model/catboost_tagger.py
+++ new/model/catboost_tagger.py
@@ -1,11 +1,12 @@
+import pickle
 from typing import List, Union
 
 import numpy as np
-from catboost import CatBoost
+from catboost import CatBoost, Pool
 
+from model.blamed_tagger import BlamedTagger
 from new.data.report import Report
-from new.model.blamed_tagger import BlamedTagger
-from new.model.features.features_computer import FeaturesComputer
+from new.model.blamed_ranker import BlamedRanker
 from new.model.report_encoders.report_encoder import ReportEncoder
 
 
@@ -23,8 +24,10 @@ class CatBoostRanker(BlamedRanker):
         'metric_period': 100
     }
 
-    def __init__(self, feature_sources: List[Union[BlamedTagger, ReportEncoder]]):
+    def __init__(self, feature_sources: List[Union[BlamedTagger, ReportEncoder]],
+                save_path: str):
         self.feature_sources = feature_sources
+        self.save_path = save_path
 
     def create_features(self, report: Report) -> List[List[float]]:
         features = self.report_encoder.encode_report(report).tolist()
@@ -34,26 +37,34 @@ class CatBoostRanker(BlamedRanker):
                 features[i].append(value)
         return features
 
+    def split_by_report_id(self, grouping, train_count):
+        last_group = -1
+        border = (0, 0)
+        for i, report_id in enumerate(grouping):
+            if report_id != last_group:
+                last_group = report_id
+                border = (border[0] + 1, border[1])
+
+            if border[0] == train_count:
+                break
+            border = (border[0], border[1] + 1)
+
+        return border[1]
+
     def train_test_splitting(self, features, targets, grouping, fraction=0.9):
         reports_count = len(list(set(grouping)))
         train_count = int(reports_count * fraction)
 
-        last_group = -1
+        border = self.split_by_report_id(grouping, train_count)
 
-        for i in grouping:
-            if i != last_group:
+        train_features, test_features = features[:border], features[border:]
+        train_targets, test_targets = targets[:border], targets[border:]
+        train_groups, test_groups = grouping[:border], grouping[border:]
 
-        train_reports, test_reports = report_ids[:train_count], report_ids[train_count:]
-        df_val = df_features[df_features['report_id'].isin(test_reports)].drop(
-            ['method_name', 'indices', 'exception_class'], axis=1)
+        train_dataset = Pool(train_features, train_targets, group_id=train_groups)
+        test_dataset = Pool(test_features, test_targets, group_id=test_groups)
 
-        df_features = df_features[df_features['report_id'].isin(train_reports)]
-        X, test_X, y, test_y = train_test_split(
-            df_features.drop(['label', 'method_name', 'indices', 'exception_class'], axis=1), df_features['label'],
-            test_size=0.1, shuffle=False)
-        train_dataset = Pool(X.drop(["report_id"], axis=1), y, group_id=X['report_id'])
-        test_dataset = Pool(test_X.drop(["report_id"], axis=1), test_y, group_id=test_X['report_id'])
-        return train_dataset, test_dataset, df_val
+        return train_dataset, test_dataset
 
     def get_features(self, reports: List[Report]):
         features = []
@@ -74,9 +85,13 @@ class CatBoostRanker(BlamedRanker):
 
         return np.vstack(features), report_ids_for_grouping
 
-    def fit(self, reports: List[Report], target: List[List[int]]) -> 'CatBoostTagger':
+    def fit(self, reports: List[Report], targets: List[List[int]]) -> 'CatBoostTagger':
         features, grouping = self.get_features(reports)
+        test_pool, train_pool = self.train_test_splitting(features, targets, grouping)
 
+        self.model = CatBoost(self.default_params)
+        self.model.fit(train_pool, eval_set=test_pool)
+        self.save(self.save_path)
         return self
 
     def predict(self, report: Report) -> List[float]:
@@ -84,8 +99,8 @@ class CatBoostRanker(BlamedRanker):
         return self.model.predict(features)
 
     def save(self, path: str):
-        self.model.save_model(path, format="onnx")
+        pickle.dump(self, open(path, "wb"))
 
     @classmethod
     def load(cls, path) -> 'CatBoostTagger':
-        pass
+        return pickle.load(open(path, 'rb'))
